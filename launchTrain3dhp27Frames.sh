03e50595b088760597fed888e89bb4c3a8f1c6bf
Miguel Lima <up202108659@up.pt>
2026-04-07 17:46:41 +0100

    test

---
+++ launchTrain3dhp27Frames.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min24gb     # Reserved partition
+#SBATCH --qos=gpu_min24gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --job-name=trainMotionAGFormer3dhp27Frames    # Job name
+#SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
+
+echo "Starting training job for MotionAGFormer on MPI-INF-3DHP"
+
+# Set wandb API key if needed
+# export WANDB_API_KEY=your_api_key_here
+
+# Commands / scripts to run (e.g., python3 train.py)
+#python3 train_3dhp.py \
+#    --config configs/mpi/TCPFormer_mpi_27.yaml \
+#    --new-checkpoint checkpoint_mpi \
+#    --use-wandb \
+#    --wandb-name "TCPFormer_MPI_3DHP_training_YOLOv11"
+
+
+python3 train_3dhp.py \
+    --config configs/mpi/MotionAGFormer-small_27.yaml \
+    --checkpoint checkpoint \
+    --checkpoint-file motionagformer-s-mpi.pth.tr \
+    --new-checkpoint checkpoint_mpi_27 \
+    --use-wandb \
+    --wandb-name "MotionAGFormer_MPI_3DHP_27_frames_training"
