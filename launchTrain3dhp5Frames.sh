2da45114fc0f7401a8e1ca8197360414d0d2998e
Miguel Lima <up202108659@up.pt>
2026-04-09 10:30:06 +0100

    nt

---
+++ launchTrain3dhp5Frames.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min24gb     # Reserved partition
+#SBATCH --qos=gpu_min24gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --job-name=trainMotionAGFormer3dhp5Frames    # Job name
+#SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
+
+echo "Starting training job for MotionAGFormer on MPI-INF-3DHP"
+
+# Set wandb API key if needed
+# export WANDB_API_KEY=your_api_key_here
+
+python3 train_3dhp.py \
+    --config configs/mpi/MotionAGFormer-small_5.yaml \
+    --new-checkpoint checkpoint_mpi_5 \
+    --use-wandb \
+    --wandb-name "MotionAGFormer_MPI_3DHP_5_frames_training"
